ac0457e8dc8c521eeb2c87af2697ab0c3e579dbd
Nikolaos Demis <nikos.demis@iteam.gr>
2018-08-08 09:42:36 +0300

    First migration

---
+++ ConditionsDataCollector/Migrations/20180808064029_initial.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ConditionsDataCollector.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alarms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Message = table.Column<string>(nullable: true),
+                    SensorTriggered = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Timestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alarms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conditions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Humidity = table.Column<float>(nullable: false),
+                    SensorName = table.Column<string>(nullable: true),
+                    Temperature = table.Column<float>(nullable: false),
+                    TimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conditions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alarms");
+
+            migrationBuilder.DropTable(
+                name: "Conditions");
+        }
+    }
+}
