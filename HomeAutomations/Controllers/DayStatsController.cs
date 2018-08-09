43fc55db3387a7b72a9f7e0c8a62834bf12cdf88
Nikolaos Demis <nikos.demis@iteam.gr>
2018-08-09 12:03:28 +0300

    delete

---
+++ HomeAutomations/Controllers/DayStatsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAutomations.Controllers
+{
+    [Route("api/[controller]")]
+    public class DayStatsController : Controller
+    {
+        // GET api/DayStats
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
